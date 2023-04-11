8befef0fd0fefa4cf7ad6c977c9a6b8f76deb2cd
Smartich0ke <41409702+Smartich0ke@users.noreply.github.com>
2023-04-12 08:11:07 +0930

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+
+/*
+ LED PINS
+
+ D2 = 8
+ D3 = 9
+ D4 = 10
+ D5 = 11
+ D6 = 12
+ D7 = 13
+ D8 = A0
+ D9 = A1
+ D10 = A2
+
+Grid pattern:
+D2 D3 D4
+D5 D6 D7
+D8 D9 D10
+*/
+
+void setup() {
+
+}
+
+void loop() {
+
+}
