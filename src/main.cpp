85a3571803729554528c59663f0b2fce706eecc0
Smartich0ke <41409702+Smartich0ke@users.noreply.github.com>
2023-04-13 07:31:37 +0930

    upload code

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,102 @@
 #include <Arduino.h>
 
-/*
- LED PINS
+const uint8_t ledPins[] = {8, 9, 10, 11, 12, 13, A0, A1, A2};
+const uint8_t buttonPins[] = {0, 1, 2, 3, 4, 5, 6, 7, A5};
+const uint8_t numPins = 9;
+const uint16_t debounceDelay = 50;
 
- D2 = 8
- D3 = 9
- D4 = 10
- D5 = 11
- D6 = 12
- D7 = 13
- D8 = A0
- D9 = A1
- D10 = A2
+uint8_t sequence[10];
+uint8_t sequenceLength = 1;
+uint32_t startTime;
 
-Grid pattern:
-D2 D3 D4
-D5 D6 D7
-D8 D9 D10
-*/
+int8_t getButtonPress(uint8_t currentIndex);
+void playSequence();
+void startNewGame();
+void signalFailure();
 
 void setup() {
-
+  for (uint8_t i = 0; i < numPins; i++) {
+    pinMode(ledPins[i], OUTPUT);
+    pinMode(buttonPins[i], INPUT_PULLUP);
+  }
+  randomSeed(analogRead(A4));
+  startNewGame();
 }
 
 void loop() {
+  playSequence();
+  uint8_t correctIndex = 0;
+  while (correctIndex < sequenceLength) {
+    int8_t pressedButton = getButtonPress(correctIndex);
+    if (pressedButton == -1) { // Timeout
+      signalFailure();
+      startNewGame();
+      return;
+    } else if (pressedButton == -2) { // Incorrect button
+      signalFailure();
+      startNewGame();
+      return;
+    } else if (pressedButton == sequence[correctIndex]) {
+      correctIndex++;
+    }
+  }
+  sequenceLength++;
+  if (sequenceLength > 10) {
+    // end-of-game logic
+    startNewGame();
+    return;
+  }
+  delay(1000);
+}
 
+void startNewGame() {
+  sequenceLength = 1;
+  for (uint8_t i = 0; i < 10; i++) {
+    sequence[i] = random(numPins);
+  }
+}
+
+void playSequence() {
+  for (uint8_t i = 0; i < sequenceLength; i++) {
+    digitalWrite(ledPins[sequence[i]], HIGH);
+    delay(500);
+    digitalWrite(ledPins[sequence[i]], LOW);
+    delay(500);
+  }
+}
+
+int8_t getButtonPress(uint8_t currentIndex) {
+  startTime = millis();
+  while (true) {
+    for (uint8_t i = 0; i < numPins; i++) {
+      if (digitalRead(buttonPins[i]) == LOW) {
+        delay(debounceDelay); // Debounce delay
+        if (digitalRead(buttonPins[i]) == LOW) {
+          while (digitalRead(buttonPins[i]) == LOW); // Wait for button release
+          if (i == sequence[currentIndex]) {
+            return i;
+          } else {
+            return -2; // Incorrect button
+          }
+        }
+      }
+    }
+    if (millis() - startTime > 5000) { // 5 seconds timeout
+      return -1;
+    }
+  }
+}
+
+void signalFailure() {
+  for (uint8_t j = 0; j <= 4; j++) {
+    for (uint8_t i = 0; i < numPins; i++) {
+      digitalWrite(ledPins[i], HIGH);
+    }
+    delay(150);
+    for (uint8_t i = 0; i < numPins; i++) {
+      digitalWrite(ledPins[i], LOW);
+    }
+    delay(150);
+  }
+  delay(500);
 }
